4645f5bfa946da85612907ec98a71da1d8026579
Jones Vitolo <jsvitolo@gmail.com>
2020-02-26 09:54:12 -0300

    Use argon2_elixir
    Configure to use elixirrc_paths and aliases

--- mix.exs
+++ mix.exs
@@ -6,8 +6,10 @@ defmodule Stonex.MixProject do
       app: :stonex,
       version: "0.1.0",
       elixir: "~> 1.9",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -19,13 +21,27 @@ defmodule Stonex.MixProject do
     ]
   end
 
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:plug_cowboy, "~> 2.1.2"},
       {:jason, "~> 1.1"},
       {:ecto_sql, "~> 3.3"},
-      {:postgrex, ">= 0.0.0"}
+      {:postgrex, ">= 0.0.0"},
+      {:argon2_elixir, "~> 2.0"}
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: ["deps.get", "ecto.setup"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
